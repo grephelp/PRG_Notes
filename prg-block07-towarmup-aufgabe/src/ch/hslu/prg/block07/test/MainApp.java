785322f6ebb0641b66b20c4d18a0ecef180ca006
grephelp <joel.rast@stud.hslu.ch>
2026-01-15 09:22:19 +0100

    Update MainApp.java
    Korrektur - Verbesserung beim Üben für PRG

--- prg-block07-towarmup-aufgabe/src/ch/hslu/prg/block07/test/MainApp.java
+++ prg-block07-towarmup-aufgabe/src/ch/hslu/prg/block07/test/MainApp.java
@@ -1,11 +1,50 @@
 package ch.hslu.prg.block07.test;
 
+import java.time.LocalDate;
+import ch.hslu.prg.block07.container.SimpleGenericList;
+import ch.hslu.prg.block07.domain.Adresse;
+
 public class MainApp {
 	
 	public static void main(String[] args) {
 		
+		SimpleGenericList<Adresse> adresseListe = new SimpleGenericList<>(3);
+		
+		SimpleGenericList<LocalDate> datumListe = new SimpleGenericList<>(2);
 		
 		
+		adresseListe.add(new Adresse());
+		adresseListe.add(new Adresse());
+		adresseListe.add(new Adresse());
+		
+		
+		boolean viertesElement =  adresseListe.add(new Adresse());
+		
+		System.out.println("Ergebnis: " + (viertesElement ? "Es hat geklappt" : "Es hat nicht geklappt"));
+		
+		int zahl = adresseListe.getSize();
+		System.out.println("Die Anzahl " + zahl);
+		
+		System.out.println();
+		
+		boolean elementLoeschen = adresseListe.remove(1);
+		System.out.println("Loeschergebnis " + (elementLoeschen ? "Es hat geklappt" : "Es hat nicht geklappt"));
+		
+		
+		int newZahl = adresseListe.getSize();
+		
+		System.out.println("Neue Anzahl des Indexes " + newZahl);
+		
+		System.out.println();
+		
+		boolean removeIndex = adresseListe.remove(10);
+		System.out.println("Ergebnis 3: " + (removeIndex ? "Das Löschen hat geklappt" : "Das Löschen hat nicht geklappt"));
+		
+		datumListe.add(LocalDate.of(2026, 01, 16));
+		datumListe.add(LocalDate.of(2026, 01, 18));
+		
+		int localDateSize = datumListe.getSize();
+		System.out.println("Die Grösse des LocalDates " + localDateSize);
 		
 		
 	}
